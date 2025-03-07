10f147ce46fccbcc1bec97aceca6ab8d579e8789
Lukas Schmelzer <lukas@familieschmelzer.de>
2025-03-07 22:00:04 +0100

    added crawling of queries and filtering of relevant links

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import asyncio
-from crawler import crawl
+from crawler import crawl, search
 from router import lm, lm_structured
+import json
+
 
 
 
 def main():
     request = input("Please enter the Topik that should be researched? \n")
-    result = lm_structured(request)
-    print(result)
+    queries = lm_structured(request)
+    filterprompt = "Please only provite the links that are relevant to the research theme: " + request + "provide them in this format: [link1, link2, link3, link4, and so on]. DO NOT PROVIDE ANY EXTRA TEXT, JUST THE LINKS. Here is the data: "
     
-
+    for i in range(1, 5):
+        query_key = f"query{i}"
+        result = asyncio.run(search(queries[query_key]))
+        print(lm(filterprompt + result))
+        
 
 #async def main():
 #    result = await crawl("https://www.example.com")
